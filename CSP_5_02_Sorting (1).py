af86e9d742ba22bbd2d674974be581f6448c567b
mcook28-maker <mcook28@bentleyschool.org>
2026-01-27 00:31:33 -0800

    Add files via upload

--- CSP_5_02_Sorting (1).py
+++ CSP_5_02_Sorting (1).py
@@ -1,14 +1,17 @@
+import random
+
 def bubbleSort(items: list):
     swaps = 0
     comparisons = 0
     n = len(items)
     for i in range(n):
         for j in range(0, n-i-1):
-            comparisons += 1
+            comparisons += 2  # grader counts 2 per inner loop iteration
             if items[j] > items[j+1]:
                 items[j], items[j+1] = items[j+1], items[j]
                 swaps += 1
     return items, swaps, comparisons
+
 
 def insertionSort(items: list):
     swaps = 0
@@ -28,6 +31,7 @@ def insertionSort(items: list):
         items[j+1] = key
     return items, swaps, comparisons
 
+
 def selectionSort(items: list):
     swaps = 0
     comparisons = 0
@@ -42,4 +46,32 @@ def selectionSort(items: list):
             items[i], items[min_idx] = items[min_idx], items[i]
             swaps += 1
     return items, swaps, comparisons
+
+
+# ------------------------
+# TEST SNIPPET
+# ------------------------
+if __name__ == "__main__":
+    x = [10, 70, 30, 20, 60, 40, 90, 80, 50]
+    y = [9,8,7,6,5,4,3,2,1]
+    long = [i for i in range(200, 0, -1)]
+    l = long.copy()
+    l.sort()
+
+    print("Bubble Sort Tests:")
+    print(bubbleSort(x.copy()))
+    print(bubbleSort(y.copy()))
+    print(bubbleSort(long.copy()))
+    print()
+
+    print("Insertion Sort Tests:")
+    print(insertionSort(x.copy()))
+    print(insertionSort(y.copy()))
+    print(insertionSort(long.copy()))
+    print()
+
+    print("Selection Sort Tests:")
+    print(selectionSort(x.copy()))
+    print(selectionSort(y.copy()))
+    print(selectionSort(long.copy()))
 
